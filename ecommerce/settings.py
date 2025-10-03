40d1dc93847626a2bb728b981bb0f37d7b139f88
muppudathiganesh <muppuraj11@gmail.com>
2025-10-03 21:39:06 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-qfr=-t#$==vk7m1gn24q7fbu!yup)6()@43tm0gt5!9^-!!&46
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
 
 
 # Application definition
